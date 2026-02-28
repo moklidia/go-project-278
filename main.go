0f6fd79d8a811698c35c5992d899d75a5880406a
Lidia Mokevnina <lidia.mokevnina@dualbootpartners.com>
2026-02-28 12:22:07 +0100

    added environment

--- main.go
+++ main.go
@@ -4,14 +4,22 @@ import (
   "net/http"
 	"log"
 
+	"github.com/getsentry/sentry-go"
+	sentrygin "github.com/getsentry/sentry-go/gin"
   "github.com/gin-gonic/gin"
 	"github.com/gin-contrib/logger"
 )
 
 func main() {
+	if err := sentry.Init(sentry.ClientOptions{
+		Dsn: "https://07ffea63ea0cf272dfcf249302f33c36@o4510963247546368.ingest.de.sentry.io/4510963256590416",
+	}); err != nil {
+		fmt.Printf("Sentry initialization failed: %v\n", err)
+	}
   router := gin.Default()
 	router.Use(logger.SetLogger())
 	router.Use(gin.Recovery())
+	router.Use(sentrygin.New(sentrygin.Options{}))
 
   router.GET("/ping", func(c *gin.Context) {
     c.JSON(http.StatusOK, gin.H{
